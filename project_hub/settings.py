02f729069a241ca6780c6b864d1b2f20edeb15be
Arun N <arunn2425@gmail.com>
2026-05-24 16:48:52 +0530

    fix allowed hosts

--- project_hub/settings.py
+++ project_hub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1jrriu_=pi(cuy$-b5ebz2ma_)vj%28e^%si-p9)(+n!@(nz%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['projecthub-3.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
